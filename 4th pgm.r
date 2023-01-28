c8a259abf1de40391b48602dd8ea8893aae764fb
pranusha2 <113331369+pranusha2@users.noreply.github.com>
2023-01-28 14:05:37 +0530

    Create 4th pgm.r

---
+++ 4th pgm.r
@@ -0,0 +1,48 @@
+Normalization is applied to data values in other to ensure that the data scales well, such that data values conforms to a certain range. 
+The output of the various normalization techniques are given below ;
+
+Given the data:
+
+200, 300, 400, 600, 1000
+1.(a) min-max normalization by setting min = 0 and max = 1
+Min - Max normalization :
+Min = minimum = 200
+Max = maximum = 1000
+Value = 200 :
+Value = 300 :
+Value = 400 :
+Value = 600 
+Value = 1000 :
+Normalized values = (0, 0.125, 0.25, 0.5, 1)
+
+(b) z-score normalization 
+
+Zscore normalization :
+Using a calculator :
+
+Mean, μ = 500
+Standard deviation = 316.227
+Value = 200 :
+Value = 300 :
+Value = 400 :
+Value = 600 :
+Value = 1000 :
+Normalized values = (-0.949, -0.632, -0.316, 0.316, 1.581)
+
+
+4d.Decimal Scaling :
+
+Maximum value = 1000
+Hence, we can divide our values by 10000
+200 / 10000 = 0.02
+
+300/1000 = 0.03
+
+400/1000 = 0.04
+
+600/1000 = 0.06
+
+1000/1000 = 0.1
+
+Hence, the Normalized values (0.02, 0.03, 0.04, 0.06, 0.1)
+
